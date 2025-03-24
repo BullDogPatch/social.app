63ebc26142e6abe95099b2f21918288f6c746bac
Craig Clayton <craig__clayton@outlook.com>
2025-03-24 17:08:21 +0000

    create community set up in supabase

---
+++ src/pages/CreateCommunity.tsx
@@ -0,0 +1,11 @@
+import CreateCommunity from '../components/CreateCommunity';
+
+const CreateCommunitytPage = () => {
+  return (
+    <div className='pt-20'>
+      <CreateCommunity />
+    </div>
+  );
+};
+
+export default CreateCommunitytPage;
